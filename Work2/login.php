3602cfe2645fe0714b493ed237054c13d4dde4fb
dlimamo <35568861+dlimamo@users.noreply.github.com>
2018-03-20 13:24:37 +0100

    fin

--- Work2/login.php
+++ Work2/login.php
@@ -2,18 +2,20 @@
     <head>
         <!-- importer le fichier de style -->
         <link href="nbproject/format.css" rel="stylesheet" type="text/css"/>
+    
     </head>
     <body>
+>
         <div id="container">
-            <!-- zone de connexion -->
+            <!-- interface de connexion -->
             
             <form action="verification.php">
                 
-                <h1>U'Teach</h1>                              
+                <h1><font size="8" face="Colonna MT" color="black"><i>U'Teach</i></font></h1>                              
                 <input type="text" placeholder="Identifiant" name="username" required>               
                 <input type="password" placeholder="Mot de passe" name="password" required>
-                <input type="submit" id='submit' value='LOGIN' >
-                <input type="submit" id='submit' value="INSCRIPTION">
+                <input type="submit" id='LOGIN' value='LOGIN' >
+                <input type="submit" id='INSCRIPTION' value="INSCRIPTION">
                 
 
             </form>
