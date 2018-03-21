b56448269ebafbfcb2e30ed2e326c63893ae2926
Cedricameline <35567190+Cedricameline@users.noreply.github.com>
2018-03-21 13:31:59 +0100

    pages Cours.

---
+++ Work2/Page_HTML/Cours.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="../Css/Cours.css" rel="stylesheet" type="text/css"/>
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <title></title>
+    </head>
+    <body>
+        <div id='container_cours'>
+            <h1>U'teach</h1>
+            <div class="topnav">
+                <input type="text" placeholder="Search..">
+            </div>
+            <div class="user"></div>
+            <button id='button' type="button" class="btn btn-secondary">Deconnexion</button>
+            <div id='conteneur_enseignants'>
+                <div id='conteneur_scrolable'>
+                    <table id='enseignants'>
+                        <tr>
+                            <td class='enseignants'>
+                                <p>Gabriel Poncé<br>Nantes</p>
+                            </td>
+                            <td class='notes'>
+                                <p>3.5</p>
+                            </td>
+                        </tr>
+                    </table>
+                </div> 
+            </div>
+        </div>
+    </body>
+</html>
